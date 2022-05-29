db9746f94195f8932a499d806c8f66f2205373fb
Guna Manasa Arjun <guna.manasa.arjun@gmail.com>
2022-05-30 01:51:16 +0530

    Fixed application runner issue to preload users

--- src/main/java/com/arjun/springpersistence/springdatajpa/SpringdatajpaApplication.java
+++ src/main/java/com/arjun/springpersistence/springdatajpa/SpringdatajpaApplication.java
@@ -1,10 +1,12 @@
 package com.arjun.springpersistence.springdatajpa;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import com.arjun.springpersistence.springdatajpa.model.User;
 import com.arjun.springpersistence.springdatajpa.model.repositories.UserRepository;
@@ -16,10 +18,11 @@ public class SpringdatajpaApplication {
 		SpringApplication.run(SpringdatajpaApplication.class, args);
 	}
 	
+	@Bean
 	public ApplicationRunner configure(UserRepository userRepo) {
 		return env -> {
-			User abc = new User("AbcDef", LocalDate.of(10, 03, 2022));
-			User def = new User("qwerty", LocalDate.of(05,06,2022));
+			User abc = new User("AbcDef", LocalDate.of(2021,Month.JANUARY,14));
+			User def = new User("qwerty", LocalDate.of(2021,Month.JUNE,	10));
 			
 			userRepo.save(abc);
 			userRepo.save(def);
