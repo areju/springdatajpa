5364883d46626a40863e26feacb59b9c19d08f98
Guna Manasa Arjun <guna.manasa.arjun@gmail.com>
2022-05-30 04:11:46 +0530

    Custom and native queries

--- src/main/java/com/arjun/springpersistence/springdatajpa/model/repositories/UserRepository.java
+++ src/main/java/com/arjun/springpersistence/springdatajpa/model/repositories/UserRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.util.Streamable;
 
 import com.arjun.springpersistence.springdatajpa.model.User;
@@ -63,6 +65,24 @@ public interface UserRepository extends JpaRepository<User, Long> {
     
     Streamable<User> findByEmailContaining(String text);
     Streamable<User> findByLevel(int level);
+    
+    /*
+     * Custom Queries
+     */
+    
+    @Query("select count(u) from User u where u.active = ?1")
+    int findNumberOfUsersByActivity(boolean active);
+     
+    @Query("select u from User u where u.level = :level and u.active = :active")
+    List<User> findByLevelAndActive(@Param("level") int level,
+                                   @Param("active") boolean active);
+     
+    @Query(value = "SELECT COUNT(*) FROM USERS WHERE ACTIVE = ?1",
+                    nativeQuery = true)
+    int findNumberOfUsersByActivityNative(boolean active);
+     
+    @Query("select u.username, LENGTH(u.email) as email_length from #{#entityName} u where u.username like %?1%")
+    List<Object[]> findByAsArrayAndSort(String text, Sort sort);
 
 
 
--- src/test/java/com/arjun/springpersistence/springdatajpa/QueryResultsTest.java
+++ src/test/java/com/arjun/springpersistence/springdatajpa/QueryResultsTest.java
@@ -1,11 +1,16 @@
 package com.arjun.springpersistence.springdatajpa;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.JpaSort;
 
 import com.arjun.springpersistence.springdatajpa.model.User;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 
 
 public class QueryResultsTest extends SpringdatajpaApplicationTests {
@@ -17,5 +22,28 @@ public class QueryResultsTest extends SpringdatajpaApplicationTests {
 			assertEquals(1, result.count());
 		};
 	}
+	
+    @Test
+    void testFindByAsArrayAndSort() {
+        List<Object[]> usersList1 =
+           userRepo.findByAsArrayAndSort("bc1", Sort.by("username"));
+        List<Object[]> usersList2 =
+           userRepo.findByAsArrayAndSort("bc1",
+               Sort.by("email_length").descending());
+        List<Object[]> usersList3 = userRepo.findByAsArrayAndSort(
+               "bc10", JpaSort.unsafe("LENGTH(u.email)"));
+        assertAll(
+                () -> assertEquals(2, usersList1.size()),
+                () -> assertEquals("abc1", usersList1.get(0)[0]),
+                () -> assertEquals(18, usersList1.get(0)[1]),
+                () -> assertEquals(2, usersList2.size()),
+                () -> assertEquals("abc10", usersList2.get(0)[0]),
+                () -> assertEquals(20, usersList2.get(0)[1]),
+                () -> assertEquals(1, usersList3.size()),
+                () -> assertEquals("abc10", usersList3.get(0)[0]),
+                () -> assertEquals(20, usersList3.get(0)[1])
+        );
+    }
+
 
 }
